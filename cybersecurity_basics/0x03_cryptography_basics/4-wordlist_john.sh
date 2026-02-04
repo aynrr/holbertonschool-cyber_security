26ace566f57cdecaa04b76164c0d2a773313b63e
Aynur <dunyamaliyevaa007@gmail.com>
2026-02-04 11:24:54 -0500

    JtR wordlist

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --format=Raw-MD5 --show "$1" | cut -d: -f2 | head -n -2 > 4-password.txt
+john --format=Raw-MD5 --show "$1" | cut -d: -f2 | grep -v "password hashes cracked" | grep . > 4-password.txt
