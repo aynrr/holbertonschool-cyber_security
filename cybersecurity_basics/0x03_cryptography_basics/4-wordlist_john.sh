4f943267256bd0fcaf2953bbe549d3ffd0a8a501
Aynur <dunyamaliyevaa007@gmail.com>
2026-02-04 11:19:54 -0500

    JtR wordlist

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
+john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
+john --format=Raw-MD5 --show "$1" | cut -d: -f2 | head -n -2 > 4-password.txt
