535b5c3f9176b4edc039665ecdb917e85adfcac0
Aynur <dunyamaliyevaa007@gmail.com>
2026-02-05 03:31:10 -0500

    NTLM

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" && john --format=nt --show "$1" | awk -F: '{print $2}' | grep -v '^$'  > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" && john --format=nt --show "$1" | awk -F: 'NR==1 {print $2}' | grep -v '^$' > 5-password.txt
